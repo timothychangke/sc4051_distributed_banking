475a72c452a8d6c8371ac2ee2c2a72af7d8b3a9d
Jing <ojing12321@gmail.com>
2026-02-27 16:02:50 +0800

    add skeleton

---
+++ client/src/networkUtils/msgSerializer.cpp
@@ -0,0 +1,12 @@
+#include "msgSerializer.h"
+
+NetworkUtils::MessageSerializer::MessageSerializer(){};
+NetworkUtils::MessageSerializer::~MessageSerializer(){};
+
+std::vector<uint8_t> serialize(const NetworkUtils::Message& message){
+
+}
+
+NetworkUtils::Message deserialize(const std::vector<uint8_t>& data){
+
+}
