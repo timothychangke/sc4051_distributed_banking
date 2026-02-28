be7082e043a6317731fec16daf07e95937c649c1
Jing <ojing12321@gmail.com>
2026-02-28 12:28:46 +0800

    add msgEncoder class

---
+++ client/include/networkUtils/msgEncoder.h
@@ -0,0 +1,18 @@
+#pragma once 
+
+#include <vector>
+#include <cstdint>
+#include <string>
+
+namespace NetworkUtils{
+class MessageEncoder{
+public:
+    MessageEncoder();
+    ~MessageEncoder();
+    
+    static std::vector<uint8_t> encode_message(const std::string& data);
+    static std::string decode_message(const std::vector<uint8_t>& data);
+    
+private:
+};
+}
---
+++ client/src/networkUtils/msgEncoder.cpp
@@ -0,0 +1,13 @@
+#include "msgEncoder.h"
+
+
+NetworkUtils::MessageEncoder::MessageEncoder(){};
+NetworkUtils::MessageEncoder::~MessageEncoder(){};
+
+static std::vector<uint8_t> encode_message(const std::string& data){
+
+}
+
+static std::string decode_message(const std::vector<uint8_t>& data){
+
+}
