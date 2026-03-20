6ea1e23e858424101b03a3d4e7566097e9846b2e
Jing <ojing12321@gmail.com>
2026-03-20 12:53:31 +0800

    debug

--- client/include/networkUtils/udpSocket.h
+++ client/include/networkUtils/udpSocket.h
@@ -18,7 +18,7 @@ public:
     virtual Result<std::monostate, Error::InternalError> send_message(const std::vector<uint8_t>& data) override;
     virtual Result<std::vector<uint8_t>, Error::InternalError> receive_message() override;
     virtual Result<std::monostate, Error::InternalError> bind_socket() override;
-    virtual std::pair<uint32_t, uint16_t> NetworkUtils::UDPSocket::get_local_info() override;
+    virtual std::pair<uint32_t, uint16_t> get_local_info() override;
     void connect_socket();
 
 };
