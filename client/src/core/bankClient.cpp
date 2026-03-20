f6b968ab8c8be3a198da1ad22623128a0b0beac8
Jing <ojing12321@gmail.com>
2026-03-20 12:45:30 +0800

    add flag

--- client/src/core/bankClient.cpp
+++ client/src/core/bankClient.cpp
@@ -301,6 +301,8 @@ void BankClient::send_to_server(const Protocol::Command& req_com) {
     // 2. build the Message struct
     Protocol::Message msg{};
     msg.type = Protocol::MessageType::Request;
+    msg.flag = flag;
+    
     if (BankClient::flag == Semantics::InvocationFlag::AT_MOST_ONCE){
         msg.id.request_id = ++current_request_id; // can use Semantics::generateRandomUint32() for actual prod 
     } else{
